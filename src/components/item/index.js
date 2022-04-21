f84d8717e0ba13fb4da6955d4ec2195709361311
bee95 <dcb95@naver.com>
2022-04-21 15:34:06 +0900

    feat: icon not  visible

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,7 +1,20 @@
-import React from "react";
+// import React from "react";
+// // import { useParams } from "react-router-dom";
+// import { Image } from "@chakra-ui/core";
 
-const Item = () => {
-  return <div>Item</div>;
-};
 
-export default Item;
+// const Item = () => {
+
+//   if(product){
+//     return(
+//       <service>
+//         <Image src="gibbresh.png" fallbackSrc="https://via.placeholder.com/150" />
+
+//       </service>
+//     );
+//   }else{
+//     return <Loading/>
+//   }
+// };
+
+// export default Item;
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { HStack } from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/core";
 
 const Nav = () => {
   return (
-    <HStack as="nav" spacing="10px">
+    <HStack as="nav" color="#3b3b3b" fontSize="16px" bg="#f6f5ef" w="100%" h="67px">
       <Link to="/">Logo</Link>
-      <Link to="/cart">cart</Link>
+      <Link to="/cart"> <Icon name="check-circle" size="24px" /></Link>
     </HStack>
   );
 };
