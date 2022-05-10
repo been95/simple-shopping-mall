5fc17b4851c9b1451fb83fca8cd899f1a2ab12be
bee95 <dcb95@naver.com>
2022-05-10 18:30:50 +0900

    feat: home add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+import Slider from "react-slick";
 import { Box, useColorModeValue, Button, Flex, Image, Text, Divider, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from "@chakra-ui/react";
 import ItemList from "../components/items";
 import { motion, useViewportScroll,useTransform} from "framer-motion";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const Home = () => {
   const bg = useColorModeValue("white", "gray.700");
   const { scrollYProgress } = useViewportScroll()
   const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
-
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
     <div>
      
@@ -28,10 +65,10 @@ const Home = () => {
         </Box>
       </Box>
       {/*인기상품*/}
-      <Box background="#F8F8FA" pt="100px" px="100px">
+      <Box background="#F8F8FA" pt="100px" px="100px" position="relative" height="500px">
         <Flex justifyContent="space-between" alignItems="start">
           <Flex alignItems="center">
-            <Box w="400px">
+            <Box w="400px" pl="140px">
               <Text fontSize="4xl" fontWeight="bold">
                 인기상품
               </Text>
@@ -42,11 +79,11 @@ const Home = () => {
             </Box>
             <Box>
               <Image src="img/best_banner.png" alt="Dan Abramov" />
-              <Divider borderColor="#FEE49F" position="absolute" w="453px" top="70px" right="200px" />
+              <Divider borderColor="#FEE49F" position="absolute" w="453px" top="150px" right="400px" />
             </Box>
           </Flex>
 
-          <Accordion defaultIndex={[0]}>
+          <Accordion defaultIndex={[0]} mt="95px" pr="130px">
             <AccordionItem>
               <h2>
                 <AccordionButton>
@@ -97,9 +134,37 @@ const Home = () => {
           </Accordion>
         </Flex>
       </Box>
+      {/* {Slider} */}
+      <Box mt="109px" >
+        <Slider {...settings}>
+          <div>
+          <Image src="img/slider_mogo_1.png" alt="Dan Abramov"/>
+          <Text  style={{marginTop:15}}>[미고]미고미니플러스5유모차</Text>
+          </div>
+          <div>
+            <Image src="img/slider_speat_2.png" alt="Dan Abramov" />
+            <Text>[루미]스펙트I 360회전카시트</Text>
+          </div>
+          <div>
+            <Image src="img/slider_migo_3.png" alt="Dan Abramov" />
+            <Text>[미고]올인원힙시트</Text>
+          </div>
+          <div>
+            <Image src="img/slider_speat_migo_4.png" alt="Dan Abramov" />
+            <Text>[루미]스펙트+[미고]미니플러스5</Text>
+          </div>
+          <div>
+            <Image src="img/slider_migi_migo_5.png" alt="Dan Abramov" />
+            <Text>[미고]미니플러스5+[미고]올인원 힙시트</Text>
+          </div>
+          <div>
+            <Image src="img/slider_migo_spaet_6.png" alt="Dan Abramov" />
+            <Text>[미고]올인원힙시트 +[루미]스펙트+ </Text>
+          </div>
+        </Slider>
+      </Box>
       {/*미니배너 */}
-  
-        <motion.div style={{ width: "100%",marginTop: "140px", scrollY}}><Image src="img/mini_banner.png" alt="Dan Abramov" w="100%" /></motion.div>
+        <motion.div style={{ width: "100%",marginTop: "212px", scrollY}}><Image src="img/mini_banner.png" alt="Dan Abramov" w="100%" /></motion.div>
       
       {/*상품 */}
       <Box align="center" mt="196px" mb="105px">
