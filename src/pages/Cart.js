c67439ae6bcabca253e08fba0fdba4b396c1859b
unknown <dcb95@naver.com>
2022-06-17 08:21:33 +0900

    feat: cart add

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import {useDispatch,useSelector} from "react-redux"
-import {changeName, increase} from "./../store/userSlice"
-import { Box,Table,Thead,Tbody,Tfoot,Tr,Th,Td,TableContainer, Button,Image} from "@chakra-ui/react";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeName, increase } from "./../store/userSlice";
+import { addCount } from "./../store";
+import { Box, Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableContainer, Button, Image } from "@chakra-ui/react";
 const Cart = () => {
-
-let state = useSelector((state) =>{return state})
-let dispath = useDispatch()
+  let state = useSelector((state) => {
+    return state;
+  });
+  let dispath = useDispatch();
 
   return (
     <Box>
       <Box>
-        {state.user.name}  {state.user.age}의 장바구니
-        <Button onClick={()=>{dispath(increase(100))}}></Button>
+        {state.user.name} {state.user.age}의 장바구니
+        <Button
+          onClick={() => {
+            dispath(increase(100));
+          }}
+        ></Button>
       </Box>
 
-
-
       <TableContainer>
-        <Table size='sm'>
+        <Table size="sm">
           <Thead>
             <Tr>
               <Th>상품정보</Th>
@@ -28,19 +32,23 @@ let dispath = useDispatch()
             </Tr>
           </Thead>
           <Tbody>
-            {
-            state.cart.map((a, i)=>
-            <Tr key={i}>
-              <Td>{state.cart[i].title}</Td>
-              <Td>{state.cart[i].sale}</Td>
-              <Td>{state.cart[i].count}</Td>
-              <Td><Button onClick={()=>{
-                dispath(changeName())
-              }}>+</Button></Td>
-              <Td isNumeric>30.48</Td>
-            </Tr>
-          )
-          }
+            {state.cart.map((a, i) => (
+              <Tr key={i}>
+                <Td>{state.cart[i].title}</Td>
+                <Td>{state.cart[i].sale}</Td>
+                <Td>{state.cart[i].count}</Td>
+                <Td>
+                  <Button
+                    onClick={() => {
+                      dispath(addCount(state.cart[i].id));
+                    }}
+                  >
+                    +
+                  </Button>
+                </Td>
+                <Td isNumeric>30.48</Td>
+              </Tr>
+            ))}
           </Tbody>
           <Tfoot>
             <Tr>
@@ -53,10 +61,7 @@ let dispath = useDispatch()
         </Table>
       </TableContainer>
     </Box>
-  )
-}
+  );
+};
 
-export default Cart
-
-
-
+export default Cart;
--- src/store.js
+++ src/store.js
@@ -1,29 +1,29 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
-import user from './store/userSlice'
-
-
-
-
-
-
-
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import user from "./store/userSlice";
 
 let cart = createSlice({
-    name : 'cart',
-    initialState : [
-      {id: 0, title: '[순성] 데일리모노 ISOFIX_카시트',sale:"298,000원", count : 1, details:"../public/img/daily_mono_details.jpg"},
-      {id: 1, title: '[다이치] 듀얼 오가닉 Larch-Fix 카시트',sale:"176,000원", count : 1, details:"../public/img/daily_mono_details.jpg"},
-    ],
-    reducers:{
-      addCount(state){
-        state[0].count++
-      }
-    }
-  })
+  name: "cart",
+  initialState: [
+    { id: 0, title: "[순성] 데일리모노 ISOFIX_카시트", sale: "298,000원", count: 1, details: "../public/img/daily_mono_details.jpg" },
+    { id: 1, title: "[다이치] 듀얼 오가닉 Larch-Fix 카시트", sale: "176,000원", count: 1, details: "../public/img/daily_mono_details.jpg" },
+  ],
+  reducers: {
+    addCount(state, action) {
+      let number = state.findIndex((a) => {
+        return a.id === action.payload;
+      });
+      state[number].count++;
+    },
+  },
+  addItem(state, action) {
+    state.push(action.payload);
+  },
+});
+export let { addCount, addItem } = cart.actions;
 
 export default configureStore({
-  reducer: { 
-    user:user.reducer,
-    cart:cart.reducer
-  }
-}) 
+  reducer: {
+    user: user.reducer,
+    cart: cart.reducer,
+  },
+});
