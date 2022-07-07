83c2823006a4e3fd5245a8181e0ca38c5996acbc
unknown <dcb95@naver.com>
2022-07-08 08:21:31 +0900

    feat:login

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box, Alert, AlertIcon, AlertDescription } from '@chakra-ui/core';
+import React from "react";
+import { Box, Alert, AlertIcon, AlertDescription } from "@chakra-ui/react";
 
 export default function ErrorMessage({ message }) {
   return (
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,31 +1,18 @@
-import React, { useState } from 'react';
-import {
-  Flex,
-  Box,
-  Heading,
-  FormControl,
-  FormLabel,
-  Input,
-  Button,
-  CircularProgress,
-  Text,
-  InputGroup,
-  InputRightElement,
-  Icon
-} from '@chakra-ui/core';
+import React, { useState } from "react";
+import { Flex, Box, Heading, FormControl, FormLabel, Input, Button, CircularProgress, Text, InputGroup, InputRightElement, Icon } from "@chakra-ui/react";
 
-import { userLogin } from '../utils/mockApi';
-import ErrorMessage from '../components/ErrorMessage';
+import { userLogin } from "../utils/mockApi";
+import ErrorMessage from "../components/ErrorMessage";
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleSubmit = async event => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     setIsLoading(true);
@@ -36,10 +23,10 @@ export default function Login() {
       setIsLoading(false);
       setShowPassword(false);
     } catch (error) {
-      setError('Invalid username or password');
+      setError("Invalid username or password");
       setIsLoading(false);
-      setEmail('');
-      setPassword('');
+      setEmail("");
+      setPassword("");
       setShowPassword(false);
     }
   };
@@ -48,23 +35,11 @@ export default function Login() {
 
   return (
     <Flex width="full" align="center" justifyContent="center">
-      <Box
-        p={8}
-        maxWidth="500px"
-        borderWidth={1}
-        borderRadius={8}
-        boxShadow="lg"
-      >
+      <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
         {isLoggedIn ? (
           <Box textAlign="center">
             <Text>{email} logged in!</Text>
-            <Button
-              variantColor="orange"
-              variant="outline"
-              width="full"
-              mt={4}
-              onClick={() => setIsLoggedIn(false)}
-            >
+            <Button variantColor="orange" variant="outline" width="full" mt={4} onClick={() => setIsLoggedIn(false)}>
               Sign out
             </Button>
           </Box>
@@ -78,53 +53,21 @@ export default function Login() {
                 {error && <ErrorMessage message={error} />}
                 <FormControl isRequired>
                   <FormLabel>Email</FormLabel>
-                  <Input
-                    type="email"
-                    placeholder="test@test.com"
-                    size="lg"
-                    onChange={event => setEmail(event.currentTarget.value)}
-                  />
+                  <Input type="email" placeholder="test@test.com" size="lg" onChange={(event) => setEmail(event.currentTarget.value)} />
                 </FormControl>
                 <FormControl isRequired mt={6}>
                   <FormLabel>Password</FormLabel>
                   <InputGroup>
-                    <Input
-                      type={showPassword ? 'text' : 'password'}
-                      placeholder="*******"
-                      size="lg"
-                      onChange={event => setPassword(event.currentTarget.value)}
-                    />
+                    <Input type={showPassword ? "text" : "password"} placeholder="*******" size="lg" onChange={(event) => setPassword(event.currentTarget.value)} />
                     <InputRightElement width="3rem">
-                      <Button
-                        h="1.5rem"
-                        size="sm"
-                        onClick={handlePasswordVisibility}
-                      >
-                        {showPassword ? (
-                          <Icon name="view-off" />
-                        ) : (
-                          <Icon name="view" />
-                        )}
+                      <Button h="1.5rem" size="sm" onClick={handlePasswordVisibility}>
+                        {showPassword ? <Icon name="view-off" /> : <Icon name="view" />}
                       </Button>
                     </InputRightElement>
                   </InputGroup>
                 </FormControl>
-                <Button
-                  variantColor="teal"
-                  variant="outline"
-                  type="submit"
-                  width="full"
-                  mt={4}
-                >
-                  {isLoading ? (
-                    <CircularProgress
-                      isIndeterminate
-                      size="24px"
-                      color="teal"
-                    />
-                  ) : (
-                    'Sign In'
-                  )}
+                <Button variantColor="teal" variant="outline" type="submit" width="full" mt={4}>
+                  {isLoading ? <CircularProgress isIndeterminate size="24px" color="teal" /> : "Sign In"}
                 </Button>
               </form>
             </Box>
@@ -135,11 +78,8 @@ export default function Login() {
   );
 }
 
-
-
 // import React from 'react'
 // import { Box,Flex, Heading,Button,FormControl,FormLabel,Input } from "@chakra-ui/react";
-
 
 // const Login = () => {
 
